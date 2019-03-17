41e11c3947cb9f49b07ec4c9fd0d3d4710a8dd6f
Dusmikeev <ddusmikeev@gmail.com>
2019-03-17 23:12:47 +0300

    Alphabet and moving character projects

---
+++ Homework_4/Project_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char userInput = Convert.ToChar(Console.ReadLine());
+            switch (userInput)
+            {
+                case 'W':
+                    Console.WriteLine("Move forward");
+                    break;
+                case 'S':
+                    Console.WriteLine("Move back");
+                    break;
+                case 'A':
+                    Console.WriteLine("Move left");
+                    break;
+                case 'D':
+                    Console.WriteLine("Move right");
+                    break;
+                default:
+                    Console.WriteLine("No proper movement keys were pressed");
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
