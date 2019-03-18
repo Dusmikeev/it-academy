cfce049b8ca6d18cf3b75048fceb3d37cf775a7e
Dusmikeev <ddusmikeev@gmail.com>
2019-03-18 23:33:29 +0300

    Three projects with array practice tasks

---
+++ Homework_5/Project_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = 5;      //resizable array
+            int randomMax = 100;    //resizable ceiling of random max numbers
+            Random random = new Random();
+            int[] userNumbers = new int[arraySize];
+            int[] randomNumbers = new int[arraySize];
+            int[] sumNumbers = new int[arraySize];
+
+            Console.WriteLine($"Please type {arraySize} numbers in console");
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                userNumbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (int number in userNumbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arraySize; i++)
+            {
+                randomNumbers[i] = random.Next(0, randomMax);
+                Console.Write(randomNumbers[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arraySize; i++)
+            {
+                sumNumbers[i] = randomNumbers[i] + userNumbers[i];
+                Console.Write(sumNumbers[i] + " ");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Homework_5/Project_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] reversedArray=new int[array.Length];
+
+            for (int i=array.Length-1, j =0; i >= 0; i--,j++)
+            {
+                reversedArray[j]=array[i];
+            }
+            for(int i =0; i<array.Length; i++)
+            {
+                array[i] = reversedArray[i];   
+            }
+            foreach (int number in array)
+            {   
+                Console.Write(number + " ");
+            }
+
+            Console.Read();
+        }
+    }
+}
