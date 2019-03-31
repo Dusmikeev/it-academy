4a323bd5db6b72aedd11173fa5d8954ddae9b77e
Dusmikeev <ddusmikeev@gmail.com>
2019-03-31 18:20:44 +0300

    Worked on my classwork. Figured out array basics and object arrays.

---
+++ lecture_6_tasks/project_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] array = { 32, 'A', "Hello" };
+            int additionalNumber = 10;
+            string additionalText = ", guys!";
+
+            foreach (object obj in array)
+            {
+                Console.WriteLine(obj);
+            }
+
+            int firstArrayElement = (int)array[0];
+            string lastArrayElement = (string)array[2];
+
+            firstArrayElement += additionalNumber;
+            lastArrayElement += additionalText;
+
+            array[0] = firstArrayElement;
+            array[2] = lastArrayElement;
+
+            foreach (object obj in array)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.Read();
+        }
+    }
+}
